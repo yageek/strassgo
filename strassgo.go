0f98653ac4cd8992e060a5c13bec86a85e7bdbd1
yageek <yannick.heinrich@gmail.com>
2014-06-17 22:30:20 +0200

    Generate file only after 3 minutesw

--- strassgo.go
+++ strassgo.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
 	. "github.com/yageek/strassgo/tools"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
+
+func generateGeoJSON() {
+	fmt.Println("Generate JSON...")
+	if traffic := NewTraffic(GetSections()[:], GetInformations()[:]); traffic == nil {
+		log.Fatal("Could not initialize kml")
+	} else {
+		traffic.TOGeoJson()
+	}
+}
 
 func main() {
 	m := martini.Classic()
@@ -20,14 +31,16 @@ func main() {
 
 	m.Get("/traffic", func() (int, string) {
 
-		if _, err := os.Stat("render/traffic.geojson"); os.IsNotExist(err) {
-			if traffic := NewTraffic(GetSections()[:], GetInformations()[:]); traffic == nil {
-				log.Fatal("Could not initialize kml")
-			} else {
-				traffic.TOGeoJson()
+		if fileInfo, err := os.Stat("render/traffic.geojson"); os.IsNotExist(err) {
+			fmt.Println("First time creating...")
+			generateGeoJSON()
+		} else {
+			currentTime := time.Now()
+			if currentTime.Sub(fileInfo.ModTime()).Seconds() > 180 {
+				fmt.Println("File to old...")
+				generateGeoJSON()
 			}
 		}
-
 		data, _ := ioutil.ReadFile("render/traffic.geojson")
 		return 200, string(data)
 	})
